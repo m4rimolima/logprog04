b0a8a41ca6fb72f21e2dc5e207175ba9f5c02a00
m4rimolima@gmail.com <m4rimolima@gmail.com>
2024-08-20 22:22:42 -0300

    commit inicial

---
+++ q03.py
@@ -0,0 +1,17 @@
+import time
+while 1:
+    print ("5...")
+    time.sleep (1)
+    print ("4...")
+    time.sleep (1)
+    print ("3...")
+    time.sleep (1)
+    print ("2...")
+    time.sleep (1)
+    print ("1...")
+    time.sleep (1)
+    print ("FELIZ ANO NOV0!!")
+    time.sleep (1)
+    print ("🕛..✨..🎉..")
+    time.sleep (2)
+    break
---
+++ q04.py
@@ -0,0 +1,3 @@
+for num in range(1, 11):
+    print("\n".join(f"{num} x {i} = {num*i}" for i in range(1, 11)))
+    print()
