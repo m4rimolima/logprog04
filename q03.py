d8f7d74035e131a07aa50c2c13e46dd3c57efba7
m4rimolima@gmail.com <m4rimolima@gmail.com>
2024-08-25 22:09:53 -0300

    laço de repetição

--- q03.py
+++ q03.py
@@ -1,17 +1,8 @@
+print('O COMPUTADOR VAI EXPLODIR EM:')
 import time
-while 1:
-    print ("5...")
+for n in range (5,0,-1):
+    print (f'{n} ...')
     time.sleep (1)
-    print ("4...")
-    time.sleep (1)
-    print ("3...")
-    time.sleep (1)
-    print ("2...")
-    time.sleep (1)
-    print ("1...")
-    time.sleep (1)
-    print ("FELIZ ANO NOV0!!")
-    time.sleep (1)
-    print ("🕛..✨..🎉..")
-    time.sleep (2)
-    break
+print ("ops.. só estva faltando as aspas no pint")
+time.sleep (1)
+print("💻..😜..🔌..🥴")
